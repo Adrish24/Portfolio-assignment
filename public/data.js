0e6ec0cfac3067d31fb088bba4c7033d2e3cb174
Adrish <adrishghosh23@gmail.com>
2025-04-24 01:09:50 +0530

    second commit 24.04.2025 1:00 AM

---
+++ public/data.js
@@ -0,0 +1,126 @@
+export const certifications = [
+  {
+    title: "Full stack development Let's begin",
+    description:
+      "Successfully completed a foundational course in Full Stack Development as a part of Full Stack Development with DSA.",
+    image: "/certificate.PNG",
+  },
+  {
+    title: "Certification of training",
+    description:
+      "Successfully completed a foundational course in Full Stack Development as a part of Full Stack Development with DSA.",
+    image: "/certificate.PNG",
+  },
+  {
+    title: "Certification of training",
+    description:
+      "Successfully completed a foundational course in Full Stack Development as a part of Full Stack Development with DSA.",
+    image: "/certificate.PNG",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "myStore",
+    description:
+      "A responsive and user-friendly E-commerce site. This site contains a homepage, product browsing and preview page, add to cart and a functioning search bar.",
+    tumbnail: "/myStore.PNG",
+    tech: ["ReactJS", "bootstrap", "tailwind CSS"],
+    features: [
+      "Responsive",
+      "User-friendly",
+      "Product browser and preview",
+      "Add to cart",
+      "Product filter & search filter",
+      "Search bar",
+    ],
+  },
+  {
+    id: 2,
+    title: "wolki-Talki",
+    description: "A web-chat App for real-time communication.",
+    tumbnail:
+      "https://res.cloudinary.com/dwrgejdob/image/upload/v1714895652/qsqfqxaewnxh5vouysdc.png",
+    tech: [
+      "Firebase",
+      "Firestore Realtime Database",
+      "Reactjs",
+      "Tailwind CSS",
+    ],
+    features: [
+      "Login System",
+      "Chat Rooms",
+      "Real-time Chat",
+      "Message Deletion",
+      "Message Reactions",
+      "Emoji Picker",
+    ],
+  },
+  {
+    id: 3,
+    title: "Insight Lab",
+    description: "A Responsive data Analysis landing page.",
+    tumbnail: "/insightlab.PNG",
+    tech: ["ReactJS", "SASS", "framer-motion"],
+    features: ["Responsive", "Smooth scrolling", "animated"],
+  },
+  {
+    id: 4,
+    title: "Vivadh",
+    description:
+      "This is an social intreaction website inspired from reddit, where users can disccuse about their problems and get solution, share there experiences in something with other users",
+    tumbnail: "/vivadh.PNG",
+    tech: [
+      "ReactJS",
+      "Redux",
+      "Tailwind CSS",
+      "Nodejs",
+      "ExpressJS",
+      "MongoDB",
+      "Cloudenery",
+    ],
+    features: [
+      "Login & signup",
+      "Layout picker",
+      "Create post",
+      "Comment",
+      "Upvote & downvote",
+      "Favourite",
+    ],
+  },
+  {
+    id: 5,
+    title: "E.Store",
+    description:
+      "A responsive and user-friendly E-commerce site. This site contains a homepage, product browsing and preview page, add to cart and a functioning search bar.",
+    tumbnail: "/e-commercesecond.PNG",
+    tech: ["ReactJS", "Redux", "bootstrap", "tailwind CSS", "firebae"],
+    features: [
+      "Responsive",
+      "User-friendly",
+      "Product browsing & preview page",
+      "Add to cart",
+      "Search bar",
+      "Login & signup",
+    ],
+  },
+  {
+    id: 6,
+    title: "QuizPro",
+    description:
+      "A quiz application where users can take quizzes on various topics and track their progress.",
+    tumbnail: "/quizapp.PNG",
+    tech: [
+      "ReactJS",
+      "Redux",
+      "Bootstrap",
+      "Tailwind CSS",
+      "Firebae",
+      "Nodejs",
+      "ExpressJS",
+      "MongoDB",
+    ],
+    features: ["Quiz", "Leaderboard"],
+  },
+];
